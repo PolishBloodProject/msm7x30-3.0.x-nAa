b6750d2e6a06df1cf80ff3082e15e39f2f71b5a9
Przemysław Błaszków <pblaszkow5@gmail.com>
2013-05-01 12:07:40 +0200

    fix register to bring camera work

--- arch/arm/mach-msm/board-semc_mogami.c
+++ arch/arm/mach-msm/board-semc_mogami.c
@@ -5367,10 +5367,12 @@ static struct platform_device wl1271_device = {
 
 static noinline void __init mogami_bt_wl1271(void)
 {
-	wake_lock_init(&st_wk_lock, WAKE_LOCK_SUSPEND, "st_wake_lock");
-        platform_device_register(&wl1271_device);
-        platform_device_register(&btwilink_device);
 
+	wake_lock_init(&st_wk_lock, WAKE_LOCK_SUSPEND, "st_wake_lock");
+	if (false) {
+		platform_device_register(&wl1271_device);
+		platform_device_register(&btwilink_device);
+	}
         return;
 }
 #endif
